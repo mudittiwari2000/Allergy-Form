4029f66b82f95b396466a81049bc9e11eea922dd
Mudit Tiwari <mudittiwari2000@gmail.com>
2020-10-28 16:26:34 +0530

    Project Completed before 28 October, 11:29PM IST.
      - The Form, as instructed, can take Multiple inputs for allergies, and give auto-suggestion in a box below according to the input.
      - The user can also click on the suggestion box's suggestion to replace his last allergy input with the suggestion, also as instructed.
      - The suggestions for the allergy names are read from a collection named 'allergies' I created on Google Firebase Firestore.
      - The suggestion list is read from the Firbase Firestore in descending order for 'frequency' column.
      - The data a user inputs, and submits, gets submitted to a collection named 'userData' on Google Firebase Firestore.
      - The name field, dateofbirth field, and allergies field were all made mandatory using required attribute as true.
    
    The task, given by the medID Team, completed!

--- src/components/Suggestion.js
+++ src/components/Suggestion.js
@@ -1,11 +1,33 @@
 import React from "react";
+import checkMatch from "../utils/checkMatch";
 
 // StyleSheets
 import "./Suggestion.css";
 
-function Suggestion({ match, lastInput, setAllergyValue }) {
+function Suggestion({
+  matches,
+  lastInput,
+  setAllergyValue,
+  allergyRef,
+  allergyArray,
+  setMatches,
+  visibility,
+  setVisibility
+}) {
+  const matchDivs = matches.map((match, index) => (
+    <div key={index} className="Suggestion__item">
+      {match}
+    </div>
+  ));
+
+  async function forceCheckMatch() {
+    await setMatches(checkMatch(allergyRef.current.value, allergyArray));
+    await allergyRef.current.focus();
+    await setVisibility(true)
+  }
+
   const onClick = (e) => {
-    let changedValue = '';
+    let changedValue = "";
     const exceptLast = lastInput.slice(0, lastInput.length - 1);
     const text = e.target.textContent;
     exceptLast.push(text);
@@ -13,12 +35,17 @@ function Suggestion({ match, lastInput, setAllergyValue }) {
       changedValue += `${allergy}, `;
     });
     setAllergyValue(changedValue);
-    console.log(exceptLast, changedValue);
+    setTimeout(forceCheckMatch, 100);
+    // forceCheckMatch()
   };
 
   return (
-    <div className="Suggestion" onClick={onClick}>
-      <span>{match}</span>
+    <div
+      className="Suggestion"
+      onClick={onClick}
+      style={{ display: visibility ? "flex" : "None" }}
+    >
+      {matchDivs}
     </div>
   );
 }
